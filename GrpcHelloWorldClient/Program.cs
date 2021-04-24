51fe9929e63ec0a7e546fa9900419e368c1e27a5
Felipe Vieira <felipe.svfx@gmail.com>
2021-04-24 11:55:49 -0300

    chore: configure Client Program

--- GrpcHelloWorldClient/Program.cs
+++ GrpcHelloWorldClient/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
 
 namespace GrpcHelloWorldClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new HelloService.HelloServiceClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest
+                {
+                    Name = "Hello FSV Client"
+                }
+            );
+            
+            Console.WriteLine("Greetings: " + reply.Message);
+            Console.ReadKey();
         }
     }
 }
